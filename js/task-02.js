48a05ecf8f909f042e4489b4afe3595044d4cee7
mikus151 <mikus151@wp.pl>
2023-06-07 23:05:09 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allIngredients = document.querySelector('#ingredients');
+
+ingredients.forEach((ingredient) => {
+  const liItem = document.createElement('li');
+  liItem.textContent = `${ingredient}`;
+  liItem.classList.add('item');
+  
+  allIngredients.append(liItem)
+});
