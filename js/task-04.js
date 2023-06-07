7b77ab6df2df163610d09659460d942a90e574e7
mikus151 <mikus151@wp.pl>
2023-06-08 01:13:23 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+const counter = document.querySelector('#counter');
+const decrement = document.querySelector('button[data-action="decrement"]');
+const increment = document.querySelector('button[data-action="increment"]');
+const value = document.querySelector('#value');
+
+
+let counterValue = 0;
+
+const decrementHandleClick = () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+};
+
+decrement.addEventListener("click", decrementHandleClick);
+
+const incrementHandleClick = () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+};
+
+increment.addEventListener("click", incrementHandleClick);
+
+
