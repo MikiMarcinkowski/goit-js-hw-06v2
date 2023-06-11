c03bf919b4ae373f8463c1f43c91991acacda124
mikus151 <mikus151@wp.pl>
2023-06-12 00:20:03 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+         alert("Uzupełnij wszystkie pola");
+    }
+
+    const userData = {
+        email: event.currentTarget.elements.email,
+        password: event.currentTarget.elements.password
+    }
+    console.log(userData);
+
+    form.reset;
+
+}
 
