6356cb3318aee84347ac8a47896bc2f17c33492b
mikus151 <mikus151@wp.pl>
2023-06-12 22:36:58 +0200

    Update task-01.js
    trrigering change to queryselectors

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,14 @@ const items = document.querySelectorAll(".item");
 console.log(`Numbers of categories: ${items.length}`);
 
 items.forEach((item) => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
+  const categoryName = item.querySelector("h2");
+
+  console.log(`Category: ${categoryName.textContent}`);
+  // diferent way to get the same thing:
+  // console.log(`Category: ${item.firstElementChild.textContent}`);
+
+  const numberOfItems = item.querySelectorAll("li");
+  console.log(`Elements: ${numberOfItems.length}`);
+  // diferent way to get the same thing:
+   // console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
