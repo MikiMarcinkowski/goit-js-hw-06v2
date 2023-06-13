eb8677a0d6fc8f0073e04800bea43bb08377362c
mikus151 <mikus151@wp.pl>
2023-06-13 12:18:08 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const div = document.querySelector('#controls');
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+
+const createBoxes = (amount) => {
+  let newBoxes = [];
+  let width = 30
+  let height = 30
+  
+  for (let i = 0; i < input.value; i += 1){
+    const addBox = document.createElement('div');
+    addBox.style.width = `${width}px`;
+    addBox.style.height = `${height}px`;
+    addBox.style.backgroundColor = getRandomHexColor();
+    width += 10;
+    height += 10;
+    
+    newBoxes.push(addBox);
+  }
+  boxes.append(...newBoxes);
+  input.value = '';
+  
+}
+createBtn.addEventListener('click', createBoxes);
+
+destroyBtn.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
